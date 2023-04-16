b1b34821ec57f0e9f11b54292c6496078d18bf3b
Petr <panhrnecpapinak@gmail.com>
2023-04-16 20:09:18 +0200

    Setup done, creating new Person working, date format need check

---
+++ EJB UBK Ukol/JPAPerson/src/main/java/model/entity/Person.java
@@ -0,0 +1,64 @@
+package model.entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the person database table.
+ * 
+ */
+@Entity
+@Table(name="person")
+@NamedQuery(name="Person.findAll", query="SELECT p FROM Person p")
+public class Person implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ID")
+	private long id;
+
+	@Column(name="Birthday")
+	private String birthday;
+
+	private String name;
+
+	private String sex;
+
+	public Person() {
+	}
+
+	public long getId() {
+		return this.id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getBirthday() {
+		return this.birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSex() {
+		return this.sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+}
--- src/main/java/com/example/ejb/SexEnum.java
+++
@@ -1,6 +0,0 @@
-package com.example.ejb;
-
-public enum SexEnum {
-    male,
-    female
-}
